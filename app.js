6de2ae3d90b3d07249244cee0b1785aa017ca3b7
phanith thor <pnthor17@gmail.com>
2018-12-15 19:04:02 +1300

    Lecture6 Implement NameCalculator

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict';
+  angular.module('myFirstApp',[])
+
+  .controller('NameCalculator', function($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function(){
+      var totalNameValue = 0;
+      $scope.totalValue = calculateNumericForString($scope.name);
+    };
+    function calculateNumericForString(string){
+      var totalStringValue = 0;
+      for(var i =0; i<string.length; i++){
+        totalStringValue+= string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+  });
+})();
